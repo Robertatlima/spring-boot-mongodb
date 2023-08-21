895baedde4baaba2d91520725f61ff2f46ce54d0
Roberta Lima <robertataisaa@gmail.com>
2023-08-21 10:13:51 -0300

    creating POST entity with nested User and projecting author data with DTO

--- src/main/java/com/springmongo/mongodb/config/Instantiation.java
+++ src/main/java/com/springmongo/mongodb/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.springmongo.mongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.springmongo.mongodb.domain.Post;
 import com.springmongo.mongodb.domain.User;
+import com.springmongo.mongodb.dto.AuthorDTO;
+import com.springmongo.mongodb.repositories.PostRepository;
 import com.springmongo.mongodb.repositories.UserRepository;
 
 @Configuration
@@ -14,18 +19,29 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
